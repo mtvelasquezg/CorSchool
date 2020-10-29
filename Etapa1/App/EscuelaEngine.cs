252da372eaa1674f303e899c9d9dc78392702acb
mtvelasquezg <mtvelasquezg@unal.edu.co>
2020-10-28 23:07:24 -0500

    random data was generated

--- Etapa1/App/EscuelaEngine.cs
+++ Etapa1/App/EscuelaEngine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using CoreEscuela.Entidades;
 
@@ -16,6 +17,41 @@ namespace CoreEscuela
         {
             Escuela = new Escuela("Platzi Academy", 2010, TiposEscuela.PreEscolar, ciudad: "Bogotá");
 
+            CargarCursos();
+            CargarAsignatura();
+            CargarAlumnos();
+            CargarEvaluaciones();
+
+        }
+
+        private void CargarEvaluaciones()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CargarAlumnos()
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+        }
+
+        private void CargarAsignatura()
+        {
+            foreach (var curso in Escuela.Cursos)
+            {
+                List<Asignatura> listaAsignaturas = new List<Asignatura>(){
+                            new Asignatura{Nombre = "Matematicas"} ,
+                            new Asignatura{Nombre = "Castellano"} ,
+                            new Asignatura{Nombre = "Ciencias Naturales"}
+                };
+                curso.Asignaturas.AddRange(listaAsignaturas);
+            }
+        }
+
+        private void CargarCursos()
+        {
             Escuela.Cursos = new List<Curso>()
             {
                 new Curso {Nombre = "101"},
